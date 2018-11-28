ae23b5949f27869ce57c45875c53def5427eee7f
Constance Bolte <31737918+boltece@users.noreply.github.com>
2018-11-28 11:56:00 -0500

    Add files via upload

---
+++ DataScience_classwork.R
@@ -0,0 +1,24 @@
+
+#### Please Excuse My Dear Aunt Sally ####
+#always remember the order of operations.. Parenthesis, exponents, multiply, divide, add, subtract!
+
+# 2 minus 10 = -8
+2 - 10
+
+# 3 time 5 = 15
+3 * 5
+
+# 9 divided by 2
+9 / 2
+
+# Be careful! 3 and 2 are multiplied first in this equation. Thus the answer is -1 after 6 is subtracted from 5
+5 - 3 * 2
+
+#If you want to subtract values first then put them in parenthesis before you multiply by 2
+(5 - 3) * 2
+
+# Ahhh, gotta love quick and easy square root code **
+4 ** 2
+
+# Don't forget exponents happen first before division. so here it is really 8/4 because 2 **2= 4 
+8 / 2 ** 2
